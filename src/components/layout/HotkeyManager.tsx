4df5c0d6e823a71dd605e9f1e03e0baf1b73dd64
Michael Joblin <mjoblin@users.noreply.github.com>
2023-02-20 12:02:48 +1300

    Improve settings: hotkeys/debug; UI cleanup; store theme in local storage

--- src/components/layout/HotkeyManager.tsx
+++ src/components/layout/HotkeyManager.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useState } from "react";
-import { Modal, Stack, useMantineTheme } from "@mantine/core";
+import React, { FC } from "react";
+import { Modal, Paper, Stack, Text, useMantineTheme } from "@mantine/core";
 import { useHotkeys } from "@mantine/hooks";
 
-import { useAppSelector } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { RootState } from "../../app/store/store";
 import {
     usePauseMutation,
@@ -10,12 +10,14 @@ import {
     useSeekMutation,
 } from "../../app/services/vibinTransport";
 import FieldValueList from "../fieldValueList/FieldValueList";
+import { setShowHotkeys } from "../../app/store/internalSlice";
 
 const SEEK_OFFSET_SECS = 10;
 
 const HotkeyManager: FC = () => {
+    const dispatch = useAppDispatch();
     const { colors } = useMantineTheme();
-    const [showHotkeyHelp, setShowHotkeyHelp] = useState<boolean>(false);
+    const { showHotkeys } = useAppSelector((state: RootState) => state.internal.application);
     const playStatus = useAppSelector((state: RootState) => state.playback.play_status);
     const duration = useAppSelector((state: RootState) => state.playback.current_track?.duration);
     const position = useAppSelector((state: RootState) => state.playback.playhead.position);
@@ -24,36 +26,69 @@ const HotkeyManager: FC = () => {
     const [seek] = useSeekMutation();
 
     useHotkeys([
-        ["h", () => setShowHotkeyHelp(!showHotkeyHelp)],
+        ["h", () => dispatch(setShowHotkeys(!showHotkeys))],
         ["j", () => seek(Math.max(position - SEEK_OFFSET_SECS, 0))],
         ["k", () => (playStatus === "play" ? pausePlayback() : resumePlayback())],
         ["l", () => duration && seek(Math.min(position + SEEK_OFFSET_SECS, duration))],
     ]);
 
-    return showHotkeyHelp ? (
+    return showHotkeys ? (
         <Modal
-            opened={showHotkeyHelp}
+            opened={showHotkeys}
             centered
             title="Hotkeys"
             overlayBlur={1}
-            onClose={() => setShowHotkeyHelp(false)}
+            onClose={() => dispatch(setShowHotkeys(false))}
         >
             <Stack spacing="md">
-                <FieldValueList
-                    fieldValues={{
-                        J: `Seek back ${SEEK_OFFSET_SECS} seconds`,
-                        K: "Play/Pause",
-                        L: `Seek forwards ${SEEK_OFFSET_SECS} seconds`,
-                        H: `Show this Help screen`,
-                    }}
-                    columnGap={10}
-                    keySize={16}
-                    keyWeight="bold"
-                    keyColor={colors.gray[1]}
-                    valueSize={16}
-                    valueWeight="normal"
-                    valueColor={colors.gray[5]}
-                />
+                <Paper p="md" radius={5} withBorder>
+                    <Stack spacing="sm">
+                        <Stack spacing={0}>
+                            <Text weight="bold">Playback</Text>
+                            <Text fz="xs" c="dimmed">
+                                Control the playhead during playback
+                            </Text>
+                        </Stack>
+                        <FieldValueList
+                            fieldValues={{
+                                J: `Seek back ${SEEK_OFFSET_SECS} seconds`,
+                                K: "Play/Pause",
+                                L: `Seek forwards ${SEEK_OFFSET_SECS} seconds`,
+                            }}
+                            columnGap={10}
+                            keySize={16}
+                            keyWeight="bold"
+                            keyColor={colors.gray[1]}
+                            valueSize={16}
+                            valueWeight="normal"
+                            valueColor={colors.gray[5]}
+                        />
+                    </Stack>
+                </Paper>
+
+                <Paper p="md" radius={5} withBorder>
+                    <Stack spacing="sm">
+                        <Stack spacing={0}>
+                            <Text weight="bold">Application</Text>
+                            <Text fz="xs" c="dimmed">
+                                Application-wide controls
+                            </Text>
+                        </Stack>
+                        <FieldValueList
+                            fieldValues={{
+                                H: `Show this Help screen`,
+                                D: `Show the Debug panel`,
+                            }}
+                            columnGap={10}
+                            keySize={16}
+                            keyWeight="bold"
+                            keyColor={colors.gray[1]}
+                            valueSize={16}
+                            valueWeight="normal"
+                            valueColor={colors.gray[5]}
+                        />
+                    </Stack>
+                </Paper>
             </Stack>
         </Modal>
     ) : null;
