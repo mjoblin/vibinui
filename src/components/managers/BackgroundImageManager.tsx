8e75cb5c5a05a7de66efe1f5929803face6fde74
mjoblin <mjoblin@users.noreply.github.com>
2023-03-31 15:49:24 +1300

    Fall back on `currentPlaybackTrack` when determining bg image url (#119)
    This adds background support for sources which don't involve local
    media or the playlist, such as AirPlay.

--- src/components/managers/BackgroundImageManager.tsx
+++ src/components/managers/BackgroundImageManager.tsx
@@ -12,22 +12,33 @@ const BackgroundImageManager: FC = () => {
     const currentTrackId = useAppSelector(
         (state: RootState) => state.playback.current_track_media_id
     );
+    const currentPlaybackTrack = useAppSelector((state: RootState) => state.playback.current_track);
 
     /**
      * Set the background image URL. This will change whenever the current track changes. If
-     * there's no current track then attempt to fall back on the current playlist entry.
+     * there's no current track then attempt to fall back on the current playlist entry and finally
+     * the currentPlaybackTrack (e.g. AirPlay).
+     *
+     * The currentlyPlayingArtUrl is used by <RootLayout> to render the background image div.
      */
     useEffect(() => {
         if (currentTrackId && trackById[currentTrackId]) {
             dispatch(setCurrentlyPlayingArtUrl(trackById[currentTrackId].album_art_uri));
-        } else if (playlist && playlist.entries && playlist.current_track_index !== undefined) {
+        } else if (
+            playlist &&
+            playlist.entries &&
+            playlist.current_track_index !== undefined &&
+            playlist.current_track_index !== -1
+        ) {
             dispatch(
                 setCurrentlyPlayingArtUrl(
                     playlist.entries[playlist.current_track_index]?.albumArtURI
                 )
             );
+        } else if (currentPlaybackTrack?.art_url) {
+            dispatch(setCurrentlyPlayingArtUrl(currentPlaybackTrack.art_url));
         }
-    }, [dispatch, currentTrackId, trackById, playlist]);
+    }, [dispatch, currentTrackId, trackById, playlist, currentPlaybackTrack]);
 
     return null;
 };
