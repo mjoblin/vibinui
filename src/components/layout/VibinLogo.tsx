f25743f61a8569a0230af30d76a2d63cab1f9ebe
Michael Joblin <mjoblin@users.noreply.github.com>
2023-04-03 11:53:18 +1200

    Update welcome message; use `<link rel="preload" ...` to preload fonts

--- src/components/layout/VibinLogo.tsx
+++ src/components/layout/VibinLogo.tsx
@@ -1,23 +1,42 @@
 import React, { FC } from "react";
-import { Flex, Text, ThemeIcon } from "@mantine/core";
+import { Center, Flex, Text, ThemeIcon, createStyles } from "@mantine/core";
 import { IconPlant2 } from "@tabler/icons";
 
 import CustomFonts from "../customFonts/CustomFonts";
 import { useAppGlobals } from "../../app/hooks/useAppGlobals";
 
+const useStyles = createStyles((theme) => ({
+    outline: {
+        border: "1px solid green",
+        borderRadius: 12,
+        width: 25,
+        height: 25,
+    },
+}));
+
 type VibinLogoProps = {
     compact?: boolean;
+    outline?: boolean;
 };
 
-const VibinLogo: FC<VibinLogoProps> = ({ compact = false }) => {
+const VibinLogo: FC<VibinLogoProps> = ({ compact = false, outline = false }) => {
+    const { classes } = useStyles();
     const { APP_ALT_FONTFACE } = useAppGlobals();
 
     if (compact) {
-        return (
-            <ThemeIcon size={24} color="green" radius="xl">
-                <IconPlant2 size="1rem" />
-            </ThemeIcon>
-        );
+        if (outline) {
+            return (
+                <Center className={classes.outline}>
+                    <IconPlant2 size="1rem" />
+                </Center>
+            );
+        } else {
+            return (
+                <ThemeIcon size={24} color="green" radius="xl">
+                    <IconPlant2 size="1rem" />
+                </ThemeIcon>
+            );
+        }
     }
 
     return (
