c1890a2211ac17f1d3e3ef81a6cc1d2012d87b04
mjoblin <mjoblin@users.noreply.github.com>
2023-04-13 14:27:22 +1200

    Fix missing useInterval changes (missed from previous PR) (#138)

--- src/app/hooks/useInterval.ts
+++ src/app/hooks/useInterval.ts
@@ -1,20 +1,11 @@
 import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
 
-import type { RootState, AppDispatch } from "../store/store";
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-// See https://redux-toolkit.js.org/tutorials/typescript
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-/**
- * https://overreacted.io/making-setinterval-declarative-with-react-hooks/
- *
- * @param callback
- * @param delay
- */
+// ================================================================================================
+// Taken from:
+//  https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+//
+// TODO: Can this be replaced with Mantine's useInterval hook.
+// ================================================================================================
 export const useInterval = (callback: any, delay: number) => {
     // TODO: Fix typing of callback.
     const savedCallback = useRef();
