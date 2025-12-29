4216fbf699abfe4f6df1dbb448c8b8a4d7e75c8f
mjoblin <mjoblin@users.noreply.github.com>
2025-12-28 18:26:51 -0800

    Don't show `<FavoriteIndicator>` if current track is not local (#298)

--- src/components/features/CurrentTrackScreen.tsx
+++ src/components/features/CurrentTrackScreen.tsx
@@ -471,7 +471,9 @@ const CurrentTrackScreen: FC = () => {
                                     </Text>
                                 </Tooltip>
                             </Box>
-                            <FavoriteIndicator media={currentTrack} size="1.2rem" />
+                            {isLocalMediaActive && (
+                                <FavoriteIndicator media={currentTrack} size="1.2rem" />
+                            )}
                         </Flex>
 
                         {(isLocalMediaActive || currentTrack.artist || currentTrack.album) && (
